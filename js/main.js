ec4ec2bfc965836bba9ad556643e80b7027adb37
Matthew Horning <forsakencrown@gmail.com>
2020-12-03 18:03:54 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var modal3 = document.getElementById("myModal3");
 var modal4 = document.getElementById("myModal4");
 var modal5 = document.getElementById("myModal5");
 var modal6 = document.getElementById("myModal6");
+var modal65 = document.getElementById("myModal6.5");
 var modal7 = document.getElementById("myModal7");
 var modal8 = document.getElementById("myModal8");
 var modal9 = document.getElementById("myModal9");
@@ -12,6 +13,18 @@ var modal10 = document.getElementById("myModal10");
 var modal11 = document.getElementById("myModal11");
 var modal12 = document.getElementById("myModal12");
 var modal13 = document.getElementById("myModal13");
+var modal14 = document.getElementById("myModal14");
+var modal15 = document.getElementById("myModal15");
+var modal16 = document.getElementById("myModal16");
+var modal17 = document.getElementById("myModal17");
+var modal18 = document.getElementById("myModal18");
+var modal19 = document.getElementById("myModal19");
+var modal20 = document.getElementById("myModal20");
+var modal21 = document.getElementById("myModal21");
+var modal22 = document.getElementById("myModal22");
+var modal23 = document.getElementById("myModal23");
+var modalEnd = document.getElementById("myModalEnd");
+
 
 // Get the button that opens the modal
 var btn1 = document.getElementById("myBtn");
@@ -20,6 +33,7 @@ var btn3 = document.getElementById("myBtn3");
 var btn4 = document.getElementById("myBtn4");
 var btn5 = document.getElementById("myBtn5");
 var btn6 = document.getElementById("myBtn6");
+var btn65 = document.getElementById("myBtn6.5");
 var btn7 = document.getElementById("myBtn7");
 var btn8 = document.getElementById("myBtn8");
 var btn9 = document.getElementById("myBtn9");
@@ -27,6 +41,17 @@ var btn10 = document.getElementById("myBtn10");
 var btn11 = document.getElementById("myBtn11");
 var btn12 = document.getElementById("myBtn12");
 var btn13 = document.getElementById("myBtn13");
+var btn14 = document.getElementById("myBtn14");
+var btn15 = document.getElementById("myBtn15");
+var btn16 = document.getElementById("myBtn16");
+var btn17 = document.getElementById("myBtn17");
+var btn18 = document.getElementById("myBtn18");
+var btn19 = document.getElementById("myBtn19");
+var btn20 = document.getElementById("myBtn20");
+var btn21 = document.getElementById("myBtn21");
+var btn22 = document.getElementById("myBtn22");
+var btn23 = document.getElementById("myBtn23");
+var btnEnd = document.getElementById("myBtnEnd");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close");
@@ -50,6 +75,9 @@ btn5.onclick = function() {
 btn6.onclick = function() {
   modal6.style.display = "block";
 }
+btn65.onclick = function() {
+  modal65.style.display = "block";
+}
 btn7.onclick = function() {
   modal7.style.display = "block";
 }
@@ -71,6 +99,39 @@ btn12.onclick = function() {
 btn13.onclick = function() {
   modal13.style.display = "block";
 }
+btn14.onclick = function() {
+  modal14.style.display = "block";
+}
+btn15.onclick = function() {
+  modal15.style.display = "block";
+}
+btn16.onclick = function() {
+  modal16.style.display = "block";
+}
+btn17.onclick = function() {
+  modal17.style.display = "block";
+}
+btn18.onclick = function() {
+  modal18.style.display = "block";
+}
+btn19.onclick = function() {
+  modal19.style.display = "block";
+}
+btn20.onclick = function() {
+  modal20.style.display = "block";
+}
+btn21.onclick = function() {
+  modal21.style.display = "block";
+}
+btn22.onclick = function() {
+  modal22.style.display = "block";
+}
+btn23.onclick = function() {
+  modal23.style.display = "block";
+}
+btnEnd.onclick = function() {
+  modalEnd.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 for (i = 0; i < span.length; i++)
@@ -82,6 +143,7 @@ for (i = 0; i < span.length; i++)
     modal4.style.display = "none";
     modal5.style.display = "none";
     modal6.style.display = "none";
+    modal65.style.display = "none";
     modal7.style.display = "none";
     modal8.style.display = "none";
     modal9.style.display = "none";
@@ -89,13 +151,16 @@ for (i = 0; i < span.length; i++)
     modal11.style.display = "none";
     modal12.style.display = "none";
     modal13.style.display = "none";
+    modal14.style.display = "none";
+    modal15.style.display = "none";
+    modal16.style.display = "none";
+    modal17.style.display = "none";
+    modal18.style.display = "none";
+    modal19.style.display = "none";
+    modal20.style.display = "none";
+    modal21.style.display = "none";
+    modal22.style.display = "none";
+    modal23.style.display = "none";
+    modalEnd.style.display = "none";
   }
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-//if (event.target == modal1 || event.target == modal2) {
-  //  modal.style.display = "none";
-  //  modal2.style.display = "none";
-//}
 }
